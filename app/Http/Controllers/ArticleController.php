70af07801ed2ed364bb4d9c9c4e7b7e7800845ca
BrayanPisuna <brayanpisuna@gmail.com>
2020-07-30 02:27:03 -0500

    #23 Validacion de Datos

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,42 +9,39 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+
+    private static $messages =[
+        'requiered'=>'El campo :attribute es obligatorio.',
+        'body.required'=> 'El body no es válido.',
+    ];
+
     public function index(){
-
-        return new ArticleCollection(Article::paginate(10));
-    }
-
-    public function show(Article $article){
-
-        return response()->json(new  ArticleResource($article),200);
+        return new ArticleCollection (Article::paginate (25));
 
     }
-    public function store(Request $request){
 
-        $validatedData = $request->validate([
-            'title' => 'required|string|unique:articles|max:255',
-            'body' => 'required',
+    public function show(Article $article) {
+        return response ()-> json(new ArticleResource($article),200);
+    }
 
-            ]);
+    public function store(Request $request, Article $article)
+    {
+        $request->validate([
+            'title'=>'required|string|unique:articles,title,'.$article->id.'|max:255',
+            'body'=>'required',
+            'category_id'=>'required|exists:category,id'
+        ], self::$messages);
 
-        $article  =Article::create($request->all());
+
+        $article =  Article::create($request->all());
         return response()->json($article, 201);
-
     }
     public function update(Request $request, Article $article){
-
-
         $article->update($request->all());
         return response()->json($article, 200);
-
     }
-
-
     public function delete(Request $request, Article $article){
-
-
-      $article->delete();
-
+        $article->delete();
         return response()->json(null, 204);
     }
 }
