f49b67b37c4376adbfc6d48b294317308cd20648
BrayanPisuna <brayanpisuna@gmail.com>
2020-07-30 00:22:03 -0500

    #17 Relaciones

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -35,7 +35,8 @@ class ArticlesTableSeeder extends Seeder
                   Article::create([
                       'title' => $faker->sentence,
                       'body' => $faker->paragraph,
-                      ]);         }
+                      'category_id' => $faker->numberBetween(1, 3)
+                  ]);         }
 
     }
 
